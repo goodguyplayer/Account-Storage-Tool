cb4ad7a8eea8a1c8c00d3ab32f0e8dc561949ba5
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-11-26 14:47:49 -0300

    Created test class

---
+++ src/test/java/com/example/passwordstoragedb/CRUD/AccountCRUDTest.java
@@ -0,0 +1,17 @@
+package com.example.passwordstoragedb.CRUD;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountCRUDTest {
+    AccountCRUD crud = new AccountCRUD();
+
+    @Disabled
+    @Test
+    public void testConstructor() throws Exception{
+        // TODO.: Test if table exists
+    }
+
+}
