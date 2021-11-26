bd67aab26215b9baca06a749b48ebf8da9f01865
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-11-26 17:11:26 -0300

    Reworked account, includes website

--- src/main/java/com/example/passwordstoragedb/CRUD/AccountCRUD.java
+++ src/main/java/com/example/passwordstoragedb/CRUD/AccountCRUD.java
@@ -1,6 +1,8 @@
 package com.example.passwordstoragedb.CRUD;
 
 import com.example.passwordstoragedb.models.Account;
+import com.example.passwordstoragedb.models.AccountList;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.sql.*;
@@ -8,6 +10,7 @@ import java.util.List;
 
 public class AccountCRUD implements CRUD<Account>, CRUDFields{
     private Connection connection;
+    static Logger logger = Logger.getLogger(AccountCRUD.class);
 
     public AccountCRUD() {
         try {
@@ -21,66 +24,177 @@ public class AccountCRUD implements CRUD<Account>, CRUDFields{
 
     @Override
     public List get(String condition) {
-        return null;
+        logger.trace("");
+        AccountList accountList = new AccountList();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(getSelectConditionalString(getTableName()) + condition);
+            while(result.next()){
+                accountList.addAccount(new Account(
+                        result.getString("website"),
+                        result.getString("username"),
+                        result.getString("email"),
+                        result.getString("password"),
+                        result.getString("reminderquestion"),
+                        result.getString("reminderanswer"),
+                        result.getBoolean("twotwofactor")
+                ));
+            }
+            result.close();
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return accountList.returnList();
     }
 
     @Override
     public List getAll() {
-        return null;
+        logger.trace("");
+        AccountList accountList = new AccountList();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(getSelectAllString(getTableName()));
+            while(result.next()){
+                accountList.addAccount(new Account(
+                        result.getString("website"),
+                        result.getString("username"),
+                        result.getString("email"),
+                        result.getString("password"),
+                        result.getString("reminderquestion"),
+                        result.getString("reminderanswer"),
+                        result.getBoolean("twotwofactor")
+                ));
+            }
+            result.close();
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
+        return accountList.returnList();
+    }
+
+    private Boolean isToReturn(int result){
+        if (result == 1){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public Boolean update(Account account) {
-        return false;
+        logger.trace("");
+        int toreturn = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getUpdateString(getTableName()));
+            preparedStatement.setString(1, account.getWebsite());
+            preparedStatement.setString(2, account.getUsername());
+            preparedStatement.setString(3, account.getEmail());
+            preparedStatement.setString(4, account.getPassword());
+            preparedStatement.setString(5, account.getReminderquestion());
+            preparedStatement.setString(6, account.getReminderanswer());
+            preparedStatement.setBoolean(7, account.getTwofactor());
+            preparedStatement.setString(8, account.getWebsite());
+            preparedStatement.setString(9, account.getUsername());
+            toreturn = preparedStatement.executeUpdate();
+        } catch (Exception e){
+            logger.error(e.getMessage());
+        }
+
+        return isToReturn(toreturn);
     }
 
     @Override
     public Boolean delete(Account account) {
-        return false;
+        logger.trace("");
+        int toreturn = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getDeleteString(getTableName()));
+            preparedStatement.setString(1, account.getWebsite());
+            preparedStatement.setString(2, account.getUsername());
+            toreturn = preparedStatement.executeUpdate();
+        } catch (Exception e){
+            logger.error(e.getMessage());
+        }
+
+        return isToReturn(toreturn);
     }
 
     @Override
     public Boolean create(Account account) {
-        return false;
+        logger.trace("");
+        int toreturn = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getInsertString(getTableName()));
+            preparedStatement.setString(1, account.getWebsite());
+            preparedStatement.setString(2, account.getUsername());
+            preparedStatement.setString(3, account.getEmail());
+            preparedStatement.setString(4, account.getPassword());
+            preparedStatement.setString(5, account.getReminderquestion());
+            preparedStatement.setString(6, account.getReminderanswer());
+            preparedStatement.setBoolean(7, account.getTwofactor());
+            toreturn = preparedStatement.executeUpdate();
+        } catch (Exception e){
+            logger.error(e.getMessage());
+        }
+
+        return isToReturn(toreturn);
     }
 
     @Override
     public String getTableName() {
+        logger.trace("");
         return "accounts";
     }
 
     @Override
     public String getDeleteString(String table) {
-        return null;
+        logger.trace("");
+        return "DELETE FROM "+ table +" WHERE website = ? AND username = ?;";
     }
 
     @Override
     public String getUpdateString(String table) {
-        return null;
+        logger.trace("");
+        return "UPDATE "+ table +" SET " +
+                "website = ?, " +
+                "username = ?, " +
+                "email = ?, " +
+                "password = ?, " +
+                "reminderquestion = ?, " +
+                "reminderanswer = ?, " +
+                "twofactor = ? " +
+                "WHERE website = ? AND username = ?;";
     }
 
     @Override
     public String getInsertString(String table) {
-        return null;
+        logger.trace("");
+        return "INSERT INTO "+ table + " (website, username, email, password, reminderquestion, reminderanswer, twofactor) VALUES (?, ?, ?, ?, ?, ?, ?);";
     }
 
     @Override
     public String getSelectAllString(String table) {
-        return null;
+        logger.trace("");
+        return "SELECT * FROM " + table;
     }
 
     @Override
     public String getSelectConditionalString(String table) {
-        return null;
+        logger.trace("");
+        return "SELECT * FROM " + table + " WHERE ";
     }
 
     private String createTableString(String table){
+        logger.trace("");
         return "CREATE TABLE IF NOT EXISTS " + table +
-                " (username VARCHAR NOT NULL," +
+                " (" +
+                " website VARCHAR NOT NULL," +
+                " username VARCHAR NOT NULL," +
                 " email VARCHAR NOT NULL," +
                 " password VARCHAR NOT NULL," +
                 " reminderquestion VARCHAR," +
                 " reminderanswer VARCHAR," +
-                " twofactor BOOLEAN);";
+                " twofactor BOOLEAN" +
+                ");";
     }
 }
--- src/main/java/com/example/passwordstoragedb/models/Account.java
+++ src/main/java/com/example/passwordstoragedb/models/Account.java
@@ -1,6 +1,7 @@
 package com.example.passwordstoragedb.models;
 
 public class Account {
+    String website;
     String username;
     String email;
     String password;
@@ -8,12 +9,69 @@ public class Account {
     String reminderanswer;
     Boolean twofactor;
 
-    public Account(String username, String email, String password, String reminderquestion, String reminderanswer, Boolean twofactor) {
+    public Account(String website, String username, String email, String password, String reminderquestion, String reminderanswer, Boolean twofactor) {
+        this.website = website;
         this.username = username;
         this.email = email;
         this.password = password;
         this.reminderquestion = reminderquestion;
         this.reminderanswer = reminderanswer;
+        this.twofactor = twofactor;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getReminderquestion() {
+        return reminderquestion;
+    }
+
+    public String getReminderanswer() {
+        return reminderanswer;
+    }
+
+    public Boolean getTwofactor() {
+        return twofactor;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setReminderquestion(String reminderquestion) {
+        this.reminderquestion = reminderquestion;
+    }
+
+    public void setReminderanswer(String reminderanswer) {
+        this.reminderanswer = reminderanswer;
+    }
+
+    public void setTwofactor(Boolean twofactor) {
         this.twofactor = twofactor;
     }
 }
