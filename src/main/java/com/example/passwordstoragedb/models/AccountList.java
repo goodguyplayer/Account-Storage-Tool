9d25b6fd69fb73a3ad5ee8a809cad5c7fb873ced
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-11-26 15:52:37 -0300

    AccountListTest updated with basic test methods

--- src/main/java/com/example/passwordstoragedb/models/AccountList.java
+++ src/main/java/com/example/passwordstoragedb/models/AccountList.java
@@ -27,5 +27,5 @@ public class AccountList {
         }
         return null;
     }
-    
+
 }
--- src/test/java/com/example/passwordstoragedb/models/AccountListTest.java
+++ src/test/java/com/example/passwordstoragedb/models/AccountListTest.java
@@ -1,5 +1,11 @@
 package com.example.passwordstoragedb.models;
 
+import com.example.passwordstoragedb.HelloApplication;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountListTest {
@@ -16,5 +22,53 @@ class AccountListTest {
             "What was your first pet2",
             "That one2",
             false);
+
+    Account account3 = new Account("username3",
+            "email2",
+            "password2",
+            "What was your first pet2",
+            "That one2",
+            false);
+
+    AccountList accountList = new AccountList();
+    static Logger logger = Logger.getLogger(AccountListTest.class);
+
+    @BeforeEach
+    public void setAccounts(){
+        logger.trace("BeforeEach method");
+        accountList.addAccount(account1);
+        accountList.addAccount(account2);
+        accountList.addAccount(account3);
+    }
+
+
+    @Test
+    public void sameAccounts(){
+        logger.trace("Test method");
+        assertSame(accountList.returnAccount(account1.username), account1);
+        assertSame(accountList.returnAccount(account2.username), account2);
+        assertSame(accountList.returnAccount(account3.username), account3);
+    }
+
+    @Test
+    public void differentAccounts(){
+        logger.trace("Test method");
+        assertNotSame(accountList.returnAccount(account2.username), account1);
+        assertNotSame(accountList.returnAccount(account2.username), account3);
+        assertNotSame(accountList.returnAccount(account1.username), account3);
+    }
+
+    @Test
+    public void returnNullIfNotFound(){
+        logger.trace("Test method");
+        assertNull(accountList.returnAccount("None"));
+    }
+
+    @Test
+    public void listNotNegative(){
+        logger.trace("Test method");
+        assertTrue(accountList.returnList().size() >= 0);
+    }
+
 
 }
